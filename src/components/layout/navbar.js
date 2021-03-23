dcb706b18f3c768af71d3d4db5beae65b8710730
denismwangi <denniske992@gmail.com>
2021-03-23 14:45:07 +0300

    dashboard

---
+++ src/components/layout/navbar.js
@@ -0,0 +1,28 @@
+import './navbar.css'
+import avatar from '../../assets/images/avatar.jpg'
+
+
+const Navbar = ({sidebarOpen, openSidebar})=> {
+    return (
+        <nav className="navbar">
+            <div className="nav-icon" onClick={() =>openSidebar()}>
+                <i className="fa fa-bars"></i>
+            </div>
+            <div className="navbar-left">
+                <a href="#">users</a>
+                <a href="#">users</a>
+                <a className="active-link" href="#">Admin</a>
+            </div>
+            <div className="navbar-right">
+                <a href="#">
+                    <i className="fa fa-search"></i>
+                </a>
+                <a href="#">
+                    <img src={avatar} width="30" />
+                </a>
+            </div>
+
+        </nav>
+    )
+}
+export default Navbar;
